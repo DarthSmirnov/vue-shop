eff1ba84bae63f139d017c3e9f53eddbf29b757f
Alexander Smirnov <darth.smirnov@yandex.ru>
2018-03-19 16:31:02 +0200

    update deps

--- server/connection.js
+++ server/connection.js
@@ -2,8 +2,6 @@
 let mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://smirnov:aqws1993@ds121534.mlab.com:21534/ng-shop', {
-    useMongoClient: true
-})
+mongoose.connect('mongodb://smirnov:aqws1993@ds121534.mlab.com:21534/ng-shop')
 
 module.exports = mongoose;
